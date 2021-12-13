9004c4c0a9ee584b381fde922745ef874e2fa23f
francis <francislow_6@hotmail.com>
2021-12-13 21:43:32 +0800

    add firestore

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import db from './firebase'
+import { collection, query, where, getDocs, onSnapshot, doc } from "firebase/firestore"
 
 function App() {
+  let [title, setTitle] = useState(null)
+
+  useEffect(() => {
+    const collectionRef = query(collection(db, "sites"), where("url", "==", window.location.hostname));
+    onSnapshot(collectionRef, (snapshots) => {
+      let { title } = snapshots.docs.map(doc => doc.data())[0]
+      setTitle(title)
+
+    })
+  }, [])
+  
   return (
     <div className="App">
       <header className="App-header">
-        <p>ELLO</p>
+        <p>{title}</p>
         <p>
           {window.location.hostname}
         </p>
