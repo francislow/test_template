3dbc664afccefdf1bda77685c0088beb52dab273
francis <francislow_6@hotmail.com>
2021-12-14 21:27:53 +0800

    no message

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import db from './firebase'
 import { collection, query, where, onSnapshot } from "firebase/firestore"
 
 function App() {
-  let [title, setTitle] = useState(null)
+  let [name, setName] = useState(null)
+  let [headline, setHeadline] = useState(null)
 
   useEffect(() => {
     const collectionRef = query(collection(db, "sites"), where("url", "==", window.location.hostname));
     onSnapshot(collectionRef, (snapshots) => {
-      let { title } = snapshots.docs.map(doc => doc.data())[0]
-      setTitle(title)
-
+      let { inputs } = snapshots.docs.map(doc => doc.data())[0]
+      let { name, headline } = inputs
+      setName(name)
+      setHeadline(headline)
     })
   }, [])
   
   return (
     <div className="App">
       <header className="App-header">
-        <p>{title}</p>
+        <p>{name ? name : 'YOUR NAME'}</p>
+        <p>{headline ? headline : "HEADLINE"}</p>
         <p>
           {window.location.hostname}
         </p>
